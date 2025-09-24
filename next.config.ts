514102b990f5a4adfc5bb3fba88e636df95f65c0
qellyka <qellyka@proton.me>
2025-09-24 20:12:29 +0300

    chore: Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -53,15 +53,15 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/:path*',
+                destination: 'http://' + process.env.NEXT_PUBLIC_BACKEND_URL + '/api/:path*',
             },
             {
                 source: '/hikes/:id/report',
-                destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/archive/hikes/:id/file/report',
+                destination: 'http://' + process.env.NEXT_PUBLIC_BACKEND_URL + '/api/archive/hikes/:id/file/report',
             },
             {
                 source: '/hikes/:id/route',
-                destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/archive/hikes/:id/file/route',
+                destination: 'http://' + process.env.NEXT_PUBLIC_BACKEND_URL + '/api/archive/hikes/:id/file/route',
             },
         ];
     },
